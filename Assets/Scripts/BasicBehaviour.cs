006b4734c6ef28c942e16071ece7796f92ba4c4a
salbrandi <salbrandi@gmail.com>
2021-10-23 15:03:14 -0400

    basic behaviour edge cases

--- Assets/Scripts/BasicBehaviour.cs
+++ Assets/Scripts/BasicBehaviour.cs
@@ -6,94 +6,129 @@ using UnityEngine;
 public class BasicBehaviour : EnemyBehaviour
 {
 
-    private float minX;
-    private float maxX;
-    private float minY;
-    private float maxY;
-    private float mapX = 30.0f;
-    private float mapY = 30.0f;
-    private Vector3 radius;
-    SpriteRenderer rend;
-    float speed = 5;
-    float attackSpeed = 0.5f; 
-    float timer = 0;
+  private float minX;
+  private float maxX;
+  private float minY;
+  private float maxY;
+  private float mapX = 30.0f;
+  private float mapY = 30.0f;
+  private Vector3 radius;
+  SpriteRenderer rend;
+  float speed = 5;
+  float attackSpeed = 0.5f;
+  float timer = 0;
 
-    public BasicBehaviour(ShipControlComponent enemyShip) : base(enemyShip) {
-      this.enemyShip = enemyShip;
+  public BasicBehaviour(ShipControlComponent enemyShip) : base(enemyShip)
+  {
+    this.enemyShip = enemyShip;
 
-      var vertExtent = Camera.main.GetComponent<Camera>().orthographicSize;
-      var horzExtent = vertExtent * Screen.width / Screen.height;
-      
-      // Calculations assume map is position at the origin
-      minX = horzExtent;
-      maxX = -horzExtent;
-      minY = vertExtent;
-      maxY = -vertExtent;
-      rend = enemyShip.gameObject.GetComponent<SpriteRenderer>();
-      // A sphere that fully encloses the bounding box.
-      radius = rend.bounds.extents;
+    var vertExtent = Camera.main.GetComponent<Camera>().orthographicSize;
+    var horzExtent = vertExtent * Screen.width / Screen.height;
 
-    }
+    // Calculations assume map is position at the origin
+    minX = horzExtent;
+    maxX = -horzExtent;
+    minY = vertExtent;
+    maxY = -vertExtent;
+    rend = enemyShip.gameObject.GetComponent<SpriteRenderer>();
+    // A sphere that fully encloses the bounding box.
+    radius = rend.bounds.extents;
 
-    public override void doAction()
+  }
+
+  public override void doAction()
+  {
+
+    Vector2 playerPos = enemyShip.gameObject.transform.position;
+
+    float topDist, botDist, leftDist, rightDist;
+
+    rightDist = Mathf.Abs(playerPos.x - maxX);
+    leftDist = Mathf.Abs(playerPos.x - minX);
+    botDist = Mathf.Abs(playerPos.y - maxY);
+    topDist = Mathf.Abs(playerPos.y - minY);
+
+    if (playerPos.x > maxX)
     {
-
-      Vector2 playerPos = enemyShip.gameObject.transform.position;
-
-      float topDist, botDist, leftDist, rightDist = 0;
-
-
-      rightDist = Mathf.Abs(playerPos.x - maxX); 
-      leftDist = Mathf.Abs(playerPos.x - minX);
-      botDist = Mathf.Abs(playerPos.y - maxY);
-      topDist = Mathf.Abs(playerPos.y - minY);
+      moveLeft();
+    }
+    else if (playerPos.x < minX)
+    {
+      moveRight();
+    }
+    else if (playerPos.y < minY)
+    {
+      moveUp();
+    }
+    else if (playerPos.y > maxY)
+    {
+      moveDown();
+    }
+    else
+    {
 
       float min = Mathf.Min(Mathf.Min(rightDist, leftDist), Mathf.Min(topDist, botDist));
 
       // TODO: more robust movement that doesnt assume the enemies are spawned on the edge 
-      if(min == rightDist){
+      if (min == rightDist)
+      {
         moveDown();
-      } else if(min == leftDist) {
-        moveUp();
-      } else if(min == botDist) {
-        moveLeft();
-      } else if(min == topDist) {
-        moveRight();
-      } 
-
-      rotateTowardsPlayer();
-
-      if (timer >= 1/attackSpeed){
-        enemyShip.getWeapon().Fire(true);
-        timer = 0;
-      } else {
-        timer += Time.deltaTime;
       }
-
+      else if (min == leftDist)
+      {
+        moveUp();
+      }
+      else if (min == botDist)
+      {
+        moveLeft();
+      }
+      else if (min == topDist)
+      {
+        moveRight();
+      }
     }
 
-    private void moveRight(){
-      enemyShip.gameObject.transform.position += new Vector3(1, 0) * Time.deltaTime * speed;
+    rotateTowardsPlayer();
+
+    if (timer >= 1 / attackSpeed)
+    {
+      enemyShip.getWeapon().Fire(true);
+      timer = 0;
+    }
+    else
+    {
+      timer += Time.deltaTime;
     }
 
-    private void moveLeft(){
-      enemyShip.gameObject.transform.position += new Vector3(-1, 0) * Time.deltaTime * speed;
-    }
+  }
 
-    private void moveUp(){
-      enemyShip.gameObject.transform.position += new Vector3(0, -1) * Time.deltaTime * speed;
+  private void moveRight()
+  {
+    enemyShip.gameObject.transform.position += new Vector3(1, 0) * Time.deltaTime * speed;
+  }
 
-    }
+  private void moveLeft()
+  {
+    enemyShip.gameObject.transform.position += new Vector3(-1, 0) * Time.deltaTime * speed;
+  }
 
-    private void moveDown(){
-      enemyShip.gameObject.transform.position += new Vector3(0, 1) * Time.deltaTime * speed;
-    }
+  private void moveUp()
+  {
+    enemyShip.gameObject.transform.position += new Vector3(0, -1) * Time.deltaTime * speed;
 
-    private void rotateTowardsPlayer(){
-      Vector3 thisToPlayer = GameManager.PlayerShip.transform.position - enemyShip.gameObject.transform.position;
-      float angle = Mathf.Atan2(thisToPlayer.y, thisToPlayer.x) * Mathf.Rad2Deg;
-      enemyShip.gameObject.transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle - 90));
-    }
+  }
+
+  private void moveDown()
+  {
+    enemyShip.gameObject.transform.position += new Vector3(0, 1) * Time.deltaTime * speed;
+  }
+
+  private void rotateTowardsPlayer()
+  {
+    Vector3 thisToPlayer = GameManager.PlayerShip.transform.position - enemyShip.gameObject.transform.position;
+    float angle = Mathf.Atan2(thisToPlayer.y, thisToPlayer.x) * Mathf.Rad2Deg;
+    enemyShip.gameObject.transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle - 90));
+  }
 
 
 }
