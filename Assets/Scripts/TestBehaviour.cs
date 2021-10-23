4d2fbb3a386e11b95f6dde901176276acf60619f
salbrandi <salbrandi@gmail.com>
2021-10-23 15:47:52 -0400

    fix for null in test scene

--- Assets/Scripts/TestBehaviour.cs
+++ Assets/Scripts/TestBehaviour.cs
@@ -4,14 +4,16 @@ using UnityEngine;
 
 public class TestBehaviour : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        ShipControlComponent enemyShipControl = gameObject.GetComponent<ShipControlComponent>();
-        BasicBehaviour enemyBehaviour = new BasicBehaviour(enemyShipControl);
-        enemyShipControl.EnemyBehaviour = enemyBehaviour;
-    }
+  // Start is called before the first frame update
+  void Start()
+  {
+    ShipControlComponent enemyShipControl = gameObject.GetComponent<ShipControlComponent>();
+    BasicBehaviour enemyBehaviour = new BasicBehaviour(enemyShipControl);
+    enemyShipControl.setNewBodyFromType(ShipBodyType.SlowChunk);
+    enemyShipControl.setNewWeaponFromType(ShipWeaponType.BulletWeapon);
+    enemyShipControl.EnemyBehaviour = enemyBehaviour;
+  }
 
-    // Update is called once per frame
-    void Update() {}
+  // Update is called once per frame
+  void Update() { }
 }
