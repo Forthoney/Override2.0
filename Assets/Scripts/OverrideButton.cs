a83778957bf4d6d1b2a9317a37a7ca4fb5ade978
salbrandi <salbrandi@gmail.com>
2021-11-20 14:50:57 -0500

    Override punctuation update

--- Assets/Scripts/OverrideButton.cs
+++ Assets/Scripts/OverrideButton.cs
@@ -2,10 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using TMPro;
+using UnityEngine.UI;
 
 public class OverrideButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-	bool _running = true;
+  bool _running = true;
   bool ishovered;
   Color startColor;
   Material startMat;
@@ -15,45 +17,47 @@ public class OverrideButton : MonoBehaviour, IPointerEnterHandler, IPointerExitH
   void Start()
   {
     startColor = new Color(14, 191, 182);
-	ShipBodySettings decorSettings = startShip.GetComponent<ShipBodySettings>();
-	if (decorSettings != null && decorSettings.Sprite != null)
-    	startMat = decorSettings.Sprite.material;
+    ShipBodySettings decorSettings = startShip.GetComponent<ShipBodySettings>();
+    if (decorSettings != null && decorSettings.Sprite != null)
+      startMat = decorSettings.Sprite.material;
   }
 
-  void OnEnable() {
-	  _running = true;
+  void OnEnable()
+  {
+    _running = true;
   }
 
   // Update is called once per frame
   void Update()
   {
-	  if (_running && startShip != null) {
-			if (ishovered)
-			{
-			//   SpriteRenderer sprite = startShip.GetComponent<SpriteRenderer>();
-			//   float rVal = Mathf.Lerp(startColor.r, 1, Time.deltaTime);
-
-			ShipBodySettings bodySettings = startShip.GetComponent<ShipBodySettings>();
-			bodySettings?.SetColor(true);
-			//   sprite.color = new Color(rVal, 0, 0);
-			//   sprite?.material?.SetColor("_GlowColor", Color.red);
-			//   sprite?.material?.SetColor("_GlowColor2", Color.red);
-			//   startShip.GetComponentsInChildren<SpriteRenderer>()[1].color = Color.red;
-			}
-			else
-			{
-			//   float rVal = Mathf.Lerp(startColor.r, 1, Time.deltaTime);
-			//   SpriteRenderer sprite = startShip.GetComponent<SpriteRenderer>();
-
-			ShipBodySettings bodySettings = startShip.GetComponent<ShipBodySettings>();
-			bodySettings?.SetColor(false);
-			//sprite.color = new Color(rVal, 0, 0);
-			//   sprite?.material?.SetColor("_GlowColor", new Color(startColor.r / 2, startColor.g / 2, startColor.b / 2, 0));
-			//   sprite?.material?.SetColor("_GlowColor2", new Color(14, 58, 191, 0));
-			//   startShip.GetComponent<SpriteRenderer>().color = startColor;
-			//   startShip.GetComponentsInChildren<SpriteRenderer>()[1].color = startColor;
-			}
-	  }
+    if (_running && startShip != null)
+    {
+      if (ishovered)
+      {
+        //   SpriteRenderer sprite = startShip.GetComponent<SpriteRenderer>();
+        //   float rVal = Mathf.Lerp(startColor.r, 1, Time.deltaTime);
+        GetComponent<TextMeshProUGUI>().SetText("\n\n                       VERRIDE!");
+        ShipBodySettings bodySettings = startShip.GetComponent<ShipBodySettings>();
+        bodySettings?.SetColor(true);
+        //   sprite.color = new Color(rVal, 0, 0);
+        //   sprite?.material?.SetColor("_GlowColor", Color.red);
+        //   sprite?.material?.SetColor("_GlowColor2", Color.red);
+        //   startShip.GetComponentsInChildren<SpriteRenderer>()[1].color = Color.red;
+      }
+      else
+      {
+        //   float rVal = Mathf.Lerp(startColor.r, 1, Time.deltaTime);
+        //   SpriteRenderer sprite = startShip.GetComponent<SpriteRenderer>();
+        GetComponent<TextMeshProUGUI>().SetText("\n\n                       VERRIDE?");
+        ShipBodySettings bodySettings = startShip.GetComponent<ShipBodySettings>();
+        bodySettings?.SetColor(false);
+        //sprite.color = new Color(rVal, 0, 0);
+        //   sprite?.material?.SetColor("_GlowColor", new Color(startColor.r / 2, startColor.g / 2, startColor.b / 2, 0));
+        //   sprite?.material?.SetColor("_GlowColor2", new Color(14, 58, 191, 0));
+        //   startShip.GetComponent<SpriteRenderer>().color = startColor;
+        //   startShip.GetComponentsInChildren<SpriteRenderer>()[1].color = startColor;
+      }
+    }
   }
 
   public void OnPointerEnter(PointerEventData pointerEventData)
@@ -67,8 +71,9 @@ public class OverrideButton : MonoBehaviour, IPointerEnterHandler, IPointerExitH
     ishovered = false;
   }
 
-  public void OnClick() {
-	  // shall we disable this on click
-	  _running = false;
+  public void OnClick()
+  {
+    // shall we disable this on click
+    _running = false;
   }
 }
